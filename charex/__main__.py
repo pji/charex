988c320cb40490670385b937976c6c60ff319e3c
Paul J. Iutzi <pji@mac.com>
2023-04-25 21:13:50 -0500

    Added denormal mode.

--- charex/__main__.py
+++ charex/__main__.py
@@ -4,17 +4,103 @@ __main__
 
 Mainline for :mod:`charex`.
 """
-from sys import argv
+from argparse import ArgumentParser, Namespace
 
 from charex.charex import Character
 
 
-c = argv[1]
-char = Character(c)
-forms = ('nfc', 'nfd', 'nfkc', 'nfkd')
-for form in forms:
-    values = char.reverse_normalize(form)
-    points = [Character(value) for value in values]
-    print(f'{form.upper()}')
-    for point in points:
-        print(f'\t{point!r}')
+def mode_denormal(args: Namespace) -> None:
+    chars = (Character(c) for c in args.base)
+    result = ''
+    for char in chars:
+        normal = char.reverse_normalize(args.form)
+        if normal:
+            result += normal[0]
+        else:
+            result += char.value
+    print(result)
+    print()
+
+
+def mode_details(args: Namespace) -> None:
+    def rev_normalize(char: Character, form: str) -> str:
+        points = char.reverse_normalize(form)
+        chars = (Character(point) for point in points)
+        values = [f'{c.value} {c.code_point} ({c.name})' for c in chars]
+        if not values:
+            return ''
+        return ('\n' + ' ' * 22).join(v for v in values)
+
+    char = Character(args.codepoint)
+
+    width = 20
+    details = (
+        ('display', char.value),
+        ('name', char.name),
+        ('code_point', char.code_point),
+        ('category', char.category),
+        ('uft-8', char.encode('utf8')),
+        ('uft-16 BE', char.encode('utf_16be')),
+        ('uft-16 LE', char.encode('utf_16le')),
+        ('uft-32 BE', char.encode('utf_32be')),
+        ('uft-32 LE', char.encode('utf_32le')),
+        ('decomposition', char.decomposition),
+        ('url encoded', char.escape('url')),
+        ('html encoded', char.escape('html')),
+        ('reverse nfc', rev_normalize(char, 'nfc')),
+        ('reverse nfd', rev_normalize(char, 'nfd')),
+        ('reverse nfkc', rev_normalize(char, 'nfkc')),
+        ('reverse nfkd', rev_normalize(char, 'nfkd')),
+    )
+
+    for detail in details:
+        label, value = detail
+        if value:
+            print(f'{label:>{width}}: {value}')
+    print()
+
+
+if __name__ == '__main__':
+    p = ArgumentParser(
+        description='Unicode and character set explorer.',
+        prog='charex'
+    )
+    sp = p.add_subparsers(required=True)
+
+    # Denormal.
+    sp_denormal = sp.add_parser(
+        'denormal',
+        aliases=['n',],
+        help='Generate strings that normalize to the given string.'
+    )
+    sp_denormal.add_argument(
+        'base',
+        help='The base normalized string.',
+        action='store',
+        type=str
+    )
+    sp_denormal.add_argument(
+        '--form', '-f',
+        help='Normalization form.',
+        default='nfkd',
+        action='store',
+        type=str
+    )
+    sp_denormal.set_defaults(func=mode_denormal)
+
+    # Details.
+    sp_details = sp.add_parser(
+        'details',
+        aliases=['d', 'deets'],
+        help='Display the details for the given code point.'
+    )
+    sp_details.add_argument(
+        'codepoint',
+        help='The code point.',
+        action='store',
+        type=str
+    )
+    sp_details.set_defaults(func=mode_details)
+
+    args = p.parse_args()
+    args.func(args)
