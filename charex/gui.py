4573455ec3b3072d521414d0f182406060d9f4b3
Paul J. Iutzi <pji@mac.com>
2023-05-21 09:12:49 -0500

    Added cl tab to GUI.

--- charex/gui.py
+++ charex/gui.py
@@ -10,6 +10,7 @@ from tkinter import ttk
 from charex import charex as ch
 from charex import charsets as cset
 from charex import util
+from charex.shell import make_description_row
 
 
 # Constants.
@@ -26,84 +27,124 @@ class CharDecode:
         root.rowconfigure(0, weight=1)
 
         book = ttk.Notebook(root, padding='1 1 1 1')
+        self.book = book
         book.grid(column=0, row=0, sticky=ALL)
         cd_frame = ttk.Frame(book, padding='3 3 12 12')
         ce_frame = ttk.Frame(book, padding='3 3 12 12')
+        cl_frame = ttk.Frame(book, padding='3 3 12 12')
         book.add(cd_frame, text='cd')
         book.add(ce_frame, text='ce')
+        book.add(cl_frame, text='cl')
 
         self.build_cd(cd_frame)
         self.build_ce(ce_frame)
+        self.build_cl(cl_frame)
+        root.bind('<Return>', self.execute)
+
+    def execute(self, *args):
+        cmds = {
+            0: self.cd,
+            1: self.ce,
+            2: self.cl,
+        }
+        tab_id = self.book.select()
+        tab = self.book.index(tab_id)
+        cmd = cmds[tab]
+        cmd()
+
+    def config_simple_grid(self, frame):
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=0)
+        frame.rowconfigure(1, weight=0)
+        frame.rowconfigure(2, weight=0)
+        frame.rowconfigure(3, weight=1)
+
+    def make_button(
+        self,
+        frame,
+        name,
+        cmd,
+        col=0,
+        row=2,
+        colspan=2,
+        rowspan=1,
+        sticky=SIDES
+    ):
+        button = ttk.Button(frame, text=name, command=cmd)
+        button.grid(
+            column=col,
+            row=row,
+            columnspan=colspan,
+            rowspan=rowspan,
+            sticky=sticky
+        )
+        return button
+
+    def make_entry(
+        self,
+        frame,
+        value,
+        width=80,
+        col=0,
+        row=1,
+        colspan=2,
+        rowspan=1,
+        sticky=SIDES,
+        justify=tk.RIGHT
+    ):
+        entry = ttk.Entry(
+            frame,
+            width=width,
+            textvariable=value,
+            justify=tk.RIGHT
+        )
+        entry.grid(
+            column=col,
+            row=row,
+            columnspan=colspan,
+            rowspan=rowspan,
+            sticky=sticky
+        )
+        return entry
+
+    def make_results(self, frame):
+        text = tk.Text(frame, width=80, height=24, wrap='word')
+        ys = ttk.Scrollbar(
+            frame,
+            orient='vertical',
+            command=text.yview
+        )
+        text['yscrollcommand'] = ys.set
+        text.grid(column=0, row=3, sticky=ALL)
+        ys.grid(column=1, row=3, sticky=ENDS)
+        return text
+
+    def pad_kids(self, frame):
+        for child in frame.winfo_children():
+            child.grid_configure(padx=2, pady=2)
 
     def build_cd(self, main):
-        main.columnconfigure(0, weight=1)
-        main.columnconfigure(1, weight=0)
-        main.rowconfigure(1, weight=0)
-        main.rowconfigure(2, weight=0)
-        main.rowconfigure(3, weight=1)
-
+        self.config_simple_grid(main)
         self.cd_address = tk.StringVar()
-        address_entry = ttk.Entry(
-            main,
-            width=80,
-            textvariable=self.cd_address,
-            justify=tk.RIGHT
-        )
-        address_entry.grid(column=0, columnspan=2, row=1, sticky=SIDES)
-
-        cd_button = ttk.Button(main, text='Decode', command=self.cd)
-        cd_button.grid(column=0, row=2, columnspan=2, sticky=SIDES)
-
-        self.cd_result = tk.Text(main, width=80, height=24, wrap='word')
-        ys = ttk.Scrollbar(
-            main,
-            orient='vertical',
-            command=self.cd_result.yview
-        )
-        self.cd_result['yscrollcommand'] = ys.set
-        self.cd_result.grid(column=0, row=3, sticky=ALL)
-        ys.grid(column=1, row=3, sticky=ENDS)
-
-        for child in main.winfo_children():
-            child.grid_configure(padx=5, pady=5)
-
-        address_entry.focus()
-        root.bind('<Return>', self.cd)
+        address_entry = self.make_entry(main, self.cd_address)
+        cd_button = self.make_button(main, 'Decode', self.cd)
+        self.cd_result = self.make_results(main)
+        self.pad_kids(main)
+        address_entry.focus_set()
 
     def build_ce(self, main):
-        main.columnconfigure(0, weight=1)
-        main.columnconfigure(1, weight=0)
-        main.rowconfigure(1, weight=0)
-        main.rowconfigure(2, weight=0)
-        main.rowconfigure(3, weight=1)
+        self.config_simple_grid(main)
+        self.ce_char = tk.StringVar()
+        char_entry = self.make_entry(main, self.ce_char)
+        cd_button = self.make_button(main, 'Encode', self.ce)
+        self.ce_result = self.make_results(main)
+        self.pad_kids(main)
 
-        self.ce_address = tk.StringVar()
-        address_entry = ttk.Entry(
-            main,
-            width=80,
-            textvariable=self.ce_address,
-            justify=tk.RIGHT
-        )
-        address_entry.grid(column=0, columnspan=2, row=1, sticky=SIDES)
-
-        ce_button = ttk.Button(main, text='Encode', command=self.ce)
-        ce_button.grid(column=0, row=2, columnspan=2, sticky=SIDES)
-
-        self.ce_result = tk.Text(main, width=80, height=24, wrap='word')
-        ys = ttk.Scrollbar(
-            main,
-            orient='vertical',
-            command=self.ce_result.yview
-        )
-        self.ce_result['yscrollcommand'] = ys.set
-        self.ce_result.grid(column=0, row=3, sticky=ALL)
-        ys.grid(column=1, row=3, sticky=ENDS)
-
-        for child in main.winfo_children():
-            child.grid_configure(padx=5, pady=5)
-
-        address_entry.focus()
-        root.bind('<Return>', self.ce)
+    def build_cl(self, frame):
+        self.config_simple_grid(frame)
+        cd_button = self.make_button(frame, 'List Character Sets', self.cl)
+        self.cl_result = self.make_results(frame)
+        self.pad_kids(frame)
 
     def cd(self, *args):
         try:
@@ -125,7 +166,8 @@ class CharDecode:
                     details = f'{char.code_point} {char.name}'
                 c = util.neutralize_control_characters(c)
                 out += f'{key:>{width}}: {c} {details}\n'
-            self.cd_result.insert('end', out)
+            out += '\n'
+            self.cd_result.insert('0.0', out)
 
         except ValueError:
             ...
@@ -133,7 +175,7 @@ class CharDecode:
     def ce(self, *args):
         try:
             codecs = cset.get_codecs()
-            base = self.ce_address.get()
+            base = self.ce_char.get()
             results = cset.multiencode(base, (codec for codec in codecs))
 
             width = max(len(codec) for codec in codecs)
@@ -142,10 +184,23 @@ class CharDecode:
                 if b := results[key]:
                     c = ' '.join(f'{n:>02x}'.upper() for n in b)
                     out += f'{key:>{width}}: {c}\n'
-            self.ce_result.insert('end', out)
+            out += '\n'
+            self.ce_result.insert('0.0', out)
 
         except ValueError:
             ...
+
+    def cl(self, *args):
+        codecs = cset.get_codecs()
+
+        out = ''
+        width = max(len(codec) for codec in codecs)
+        for codec in codecs:
+            descr = cset.get_codec_description(codec)
+            row = make_description_row(codec, width, descr)
+            out += row + '\n\n'
+        out += '\n\n'
+        self.cl_result.insert('0.0', out)
 
 
 if __name__ == '__main__':
