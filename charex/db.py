8c04a690d54ad5fcec6d5745f27167aebe6fff78
Paul J. Iutzi <pji@mac.com>
2023-06-14 07:49:39 -0500

    Still working on db.

--- charex/db.py
+++ charex/db.py
@@ -13,6 +13,7 @@ from zipfile import ZipFile
 
 
 PKG_DATA = files('charex.data')
+FILE_META = 'meta.json'
 FILE_SOURCES = 'props.json'
 MISSING_PREFIX = '# @missing: '
 MISSING_DELIM = ';'
@@ -44,6 +45,14 @@ class PropSource:
     path: str
     kind: str
     delim: str
+
+
+@dataclass
+class PropertyValue:
+    property: str
+    alias: str
+    long: str
+    other: tuple[str, ...]
 
 
 @dataclass
@@ -94,9 +103,23 @@ class UCD:
     stc: str
 
 
+# Default value callables for defaultdicts.
+class DefaultStr:
+    """A default string value callable."""
+    def __init__(self, value: str = '') -> None:
+        self.value = value
+    
+    def __call__(self) -> str:
+        return self.value
+
+
+# Types for Unicode metadata.
+FileProps = dict[str, tuple[str, ...]]
+PropValAliases = dict[str, dict[str, str]]
+SingleValue = dict[defaultdict[str, str]]
+
 # Types for Unicode files.
 UnicodeData = dict[str, UCD]
-
 
 # Generic types.
 Lines = tuple[str, ...]
@@ -105,7 +128,88 @@ Record = tuple[str, ...]
 Records = tuple[Record, ...]
 
 
+# Loaded data files.
+class Cache:
+    """A cache for storing data loaded from Unicode data files."""
+    def __init__(self) -> None:
+        # Unicode metadata.
+        self.__file_props: FileProps = {}
+        self.__metas: PropSources = {}
+        self.__sources: PropSources = {}
+        
+        # Unicode property data.
+        self.__propvalaliases: PropValAliases = {}
+    
+    @property
+    def file_props(self) -> FileProps:
+        if not self.__file_props:
+            self.__file_props = build_file_props()
+        return self.__file_props
+
+    @property
+    def meta(self) -> PropSources:
+        if not self.__metas:
+            self.__metas = load_meta_file()
+        return self.__metas
+    
+    @property
+    def sources(self) -> PropSources:
+        if not self.__sources:
+            self.__sources = load_props_file()
+        return self.__sources
+
+    @property
+    def value_aliases(self) -> PropValAliases:
+        if not self.__propvalaliases:
+            src = self.meta['valuealiases']
+            data = get_property_aliases(src)
+            for key in data:
+                self.__propvalaliases[key] = data[key]
+        return self.__propvalaliases
+    
+
+class Database:
+    """The data loaded from Unicode data files."""
+    cache = Cache()
+    
+    def __getattr__(self, name: str):
+        return getattr(self.cache, name)
+
+
 # Read the Unicode data files.
+def get_property_aliases(src: PropSource) -> PropValAliases:
+    """Get the mapping of property value long names to aliases."""
+    records, _ = get_source(src)
+    data = {}
+    for rec in records:
+        prop, alias, long, *other = rec
+        prop = prop.casefold()
+        data.setdefault(prop, dict())
+        data[prop][long] = alias
+    return data
+
+
+def get_single_values(src:PropSource) -> SingleValue:
+    """Get the data from a data file with single value properties."""
+    records, missing = get_source(src)
+    prop = db.file_props[src.path][0]    
+
+    mval = ''
+    if missing:
+        long = missing[0][-1]
+        mval = alias_value(prop, long)
+    ds = DefaultStr(mval)
+    
+    values = defaultdict(ds)
+    for rec in records:
+        codes, long = rec
+        alias = alias_value(prop, long)
+        for n in parse_range(codes):
+            code = f'{n:04x}'.casefold()
+            values[code] = alias
+    return values
+
+
 def get_unicode_data(src: PropSource) -> UnicodeData:
     """Get the data from UnicodeData.txt."""
     def fill_range(
@@ -135,6 +239,61 @@ def get_unicode_data(src: PropSource) -> UnicodeData:
             for ud in fill_range(start, stop, fields):
                 data[ud.address.casefold()] = ud
     return data
+
+
+# Unicode data utilities.
+def alias_value(prop: str, long: str) -> str:
+    """Return the alias for the long name of a property value."""
+    db = Database()
+    alias = long
+    try:
+        alias = db.value_aliases[prop][long]
+    except KeyError:
+        pass
+    return alias
+
+
+def build_jamo_name(code: str | int) -> str:
+    """Return the Jamo name for the code point."""
+    if isinstance(code, str):
+        code = int(code, 16)
+    parts = decompose_hangul(code)
+    
+
+
+def decompose_hangul(s: int) -> tuple[int, ...]:
+    """See Unicode 3.12."""
+    SBASE = 0xac00
+    LBASE = 0x1100
+    VBASE = 0x1161
+    TBASE = 0x11a7
+    LCOUNT = 19
+    VCOUNT = 21
+    TCOUNT = 28
+    NCOUNT = VCOUNT * TCOUNT
+    SCOUNT = LCOUNT * NCOUNT
+
+    sindex = s - SBASE
+    lindex = sindex // NCOUNT
+    vindex = (sindex % NCOUNT) // TCOUNT
+    tindex = sindex % TCOUNT
+
+    return (
+        LBASE + lindex,
+        VBASE + vindex,
+        TBASE + tindex,
+    )
+
+
+# Metadata construction utilities.
+def build_file_props() -> FileProps:
+    db = Database()
+    file_props = {}
+    for prop in db.sources:
+        src = db.sources[prop]
+        file_props.setdefault(src.path, list())
+        file_props[src.path].append(prop)
+    return {path: tuple(file_props[path]) for path in file_props}
 
 
 # File parsing utilities.
@@ -189,6 +348,17 @@ def strip_comments(lines: Sequence[str]) -> Lines:
 
 
 # File reading.
+def load_meta_file() -> PropSources:
+    """Load the map of Unicode property metadata to their location
+    in the Unicode data files.
+    """
+    path = PKG_DATA / FILE_META
+    fh = path.open()
+    json = load(fh)
+    fh.close()
+    return {key: PropSource(*json[key]) for key in json}
+
+
 def load_props_file() -> PropSources:
     """Load the map of Unicode character properties to their location
     in the Unicode data files.
@@ -212,13 +382,18 @@ def read_resource(src: PropSource, codec: str = 'utf8') -> Lines:
 
 
 if __name__ == '__main__':
-    sources = load_props_file()
+    db = Database()
+    src = db.sources['jsn']
+    data = get_single_values(src)
+    # data = db.file_props
+    # data = get_property_aliases(db.meta['valuealiases'])
     # lines = read_resource(sources['blk'])
     # lines = strip_comments(lines)
     # records = split_fields(lines, sources['age'])
     # vrange = parse_range(records[-1][0])
     # missing = parse_missing(lines)
     # data = get_unicode_data(sources['na'])
-    data = get_single_values(sources['jsn'])
+    # data = get_single_values(sources['jsn'])
+    # data = aliases
     for item in data:
-        print(item, ucds[item])
+        print(item, data[item])
